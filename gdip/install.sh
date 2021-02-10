511a2b5414f6ff9ba6458e2e2601e6fbb0496835
Petr Vana <vanapet1@fel.cvut.cz>
2021-02-10 21:45:08 +0100

    Update install.sh

--- gdip/install.sh
+++ gdip/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-cd build && cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=.. ../src ..
+cd build && cmake -D CMAKE_INSTALL_PREFIX=.. ../src ..
 cd -
 cmake --build build
