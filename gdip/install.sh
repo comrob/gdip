e1bf7229740b7a6724b53c660bece18f9a0fe251
Petr Vana <petvana>
2021-02-11 11:11:41 +0100

    Update install script for Win

--- gdip/install.sh
+++ gdip/install.sh
@@ -3,5 +3,13 @@
 mkdir -p build
 cd build && cmake -D CMAKE_INSTALL_PREFIX=.. ../src ..
 cd -
-cmake --build build --config Release
-cmake --install build --config Release
+
+if [[ "$OSTYPE" == "win32" ]]; then
+    cmake --build build --config Release
+    cmake --install build --config Release
+else
+    cmake --build build
+    cmake --install build
+fi
+
+
