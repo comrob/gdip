68ccfe14fcbc401ba9591c265bf8f6a7430f0812
Petr Vana <petvana>
2021-02-11 11:33:35 +0100

    Fix

--- gdip/install.sh
+++ gdip/install.sh
@@ -4,18 +4,18 @@ mkdir -p build
 cd build && cmake -D CMAKE_INSTALL_PREFIX=.. ../src ..
 cd -
 
-if [[ "$OSTYPE" == "win32" ]]; then
-    echo "Compiling on Windows"
+env
+
+UNAME=$(uname)
+
+if [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
+	echo "Compiling on Windows"
     echo "cmake --build build --config Release"
     cmake --build build --config Release
     echo "cmake --install build --config Release"
     cmake --install build --config Release
 else
     echo "Compiling Linux/OSX"
-    echo "cmake --build build"
-    cmake --build build
-    echo "cmake --install build"
-    cmake --install build
+    echo "make install -j4"
+    make -C build install -j4
 fi
-
-
