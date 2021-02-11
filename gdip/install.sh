2a82ee0a5aec76c6c1e16e9b04149d90d97e07c9
Petr Vana <petvana>
2021-02-11 11:18:38 +0100

    Fix

--- gdip/install.sh
+++ gdip/install.sh
@@ -5,10 +5,16 @@ cd build && cmake -D CMAKE_INSTALL_PREFIX=.. ../src ..
 cd -
 
 if [[ "$OSTYPE" == "win32" ]]; then
+    echo "Compiling on Windows"
+    echo "cmake --build build --config Release"
     cmake --build build --config Release
+    echo "cmake --install build --config Release"
     cmake --install build --config Release
 else
+    echo "Compiling Linux/OSX"
+    echo "cmake --build build"
     cmake --build build
+    echo "cmake --install build"
     cmake --install build
 fi
 
